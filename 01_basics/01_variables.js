98dbbd9f7db2c6b758afcb981235bef1365f5d62
UsmanCodes1 <150183444+UsmanCodes1@users.noreply.github.com>
2023-11-11 20:55:23 +0000

    Variable

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,12 @@
+const accountId= "12345"
+var accountEmail="hello@gmail.com"
+let accountPassword= "123456789"
+accountCity="Sukkkur"
+let accountState;
+
+accountEmail="hy@accountEmail.com"
+accountPassword="1333"
+accountCity="karachi"
+
+console.log(accountId);
+ console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
