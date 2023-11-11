3a7e21042c647609e1fca20f9fe1554f540fa7e5
UsmanCodes1 <150183444+UsmanCodes1@users.noreply.github.com>
2023-11-11 20:58:43 +0000

    Variables

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -1,8 +1,11 @@
 const accountId= "12345"
 var accountEmail="hello@gmail.com"
+// Prefer not to use var because of issue in block scope and functional scope
 let accountPassword= "123456789"
 accountCity="Sukkkur"
 let accountState;
+
+
 
 accountEmail="hy@accountEmail.com"
 accountPassword="1333"
