6c46e25b89e45fa89ad447648582e734ed63a6be
UsmanCodes1 <150183444+UsmanCodes1@users.noreply.github.com>
2023-11-12 15:55:31 +0000

    Datatype

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,26 @@
+"use strict" // treat all Js code newer version
+
+// alert(3+3) We are using node js, not browser
+
+console.log(3+3) //code readability should be high
+console.log("Usman");
+
+let name="Khalid"
+let age=18
+let isloggedIn =false
+let Undefined;
+
+/*
+Data Types
+Number => 2 to power 53
+Bigint
+String =>
+Boolean =>True/False
+Null => Standalone value
+Undefined =>
+symbol => Unique
+
+object
+*/
+
+console.log(typeof null);
---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,78 @@
+// let score="33abs"
+// let score= null
+// let score=undefined
+let score=false
+// console.log(score);
+// console.log(typeof score);
+// console.log(typeof (score));
+
+let valueInNumber= Number(score)
+// console.log(valueInNumber);
+// console.log(typeof valueInNumber);
+
+// "Convert in to Number  33" => 33   
+// Convert in to Number  "33abc" => Nan    Not a Number 
+// true =1; false=0;
+// "Usman"=True; "" = False
+
+let isLoggedIn= 1
+  let booleanIsLoggedIn=Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+ let string=33
+ let convertString=String(string)
+//  console.log(convertString)
+//  console.log(typeof convertString);
+
+ //  *******************************************   Operations ****************************************************
+
+let value=-3
+let negvalue = -value
+// console.log(negvalue);
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2**3)
+// console.log(2/2)
+// console.log(2%2)
+
+let str1= "Hello"
+let str2= "Usman"
+let str3= str1 + str2
+// console.log(str3)
+
+// console.log("2"+2)  22
+// console.log(2+"3")  23
+// console.log("2"+"2")  22
+// console.log("2"+2+2)  222
+// console.log(2+2+"2")  42
+// console.log((2+3)*4%5) 
+
+// console.log(+true)  1
+// console.log(+false) 0
+
+
+// let num1, num2, num3
+// num1=num2=num3=2+2
+
+let gameCounter=100
+++gameCounter;
+console.log(gameCounter);
+
+
+
+
+
+
+// Increment Operator
+// let x = 3;
+// const y = x++;  Postfix
+// console.log(x,y)
+// // x is 4; y is 3
+
+// let x2 = 3n;
+// const y2 = ++x2; Prefix
+// console.log(x2,y2);
+// // x2 is 4n; y2 is 4n
+
