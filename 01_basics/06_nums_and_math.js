03833abd1a10a495e342e0cef81751aec217d033
UsmanCodes1 <150183444+UsmanCodes1@users.noreply.github.com>
2023-11-15 18:58:13 +0000

    Number and Math.js

---
+++ 01_basics/06_nums_and_math.js
@@ -0,0 +1,33 @@
+// const score=400
+// console.log(typeof score);
+
+// const balance = new Number (100)
+// console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+
+// const otherNumber=120.2589
+// console.log(otherNumber.toPrecision(4));
+
+// const hundreds = 10000000
+// console.log(hundreds.toLocaleString('en-PK'));
+
+// +++++++++++++++++++++++++++ Maths  +++++++++++++++++++++++++++++++++++
+
+// console.log(Math)
+// console.log(Math.abs(-4));
+// console.log(Math.round(59.2));
+// console.log(Math.ceil(59.2));
+// console.log(Math.floor(59.9));
+// console.log(Math.min(3,5,2,1,5));
+// console.log(Math.max(3,5,2,1,5));
+
+console.log(Math.random());
+console.log((Math.random()*10)+1);
+console.log(Math.floor((Math.random()*10)+1));
+
+const min=10
+const max=20
+
+console.log(Math.floor(Math.random() * (max-min+1))+min);
