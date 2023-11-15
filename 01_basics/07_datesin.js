e3532a843c3af2d7d0a8f86ffe01fe0dcc351246
UsmanCodes1 <150183444+UsmanCodes1@users.noreply.github.com>
2023-11-15 20:54:36 +0000

    Date in javascript

---
+++ 01_basics/07_datesin.js
@@ -0,0 +1,22 @@
+let myDate =new Date()
+// console.log(myDate);
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toISOString());
+// console.log(myDate.toJSON());
+// console.log(myDate.toLocaleDateString());
+// console.log(myDate.toLocaleString());
+// console.log(myDate.toLocaleTimeString());
+// console.log(myDate.toTimeString());
+// console.log(myDate.toUTCString());
+
+let newDate = new Date(2023 , 8 , 17 , 5 , 25)
+console.log(newDate.toLocaleString());
+
+let myTimeStamp=Date.now()
+// console.log(myTimeStamp);
+// console.log(Math.floor(Date.now()/1000));
+
+let currentDate = new Date();
+console.log(currentDate);
+console.log(currentDate.getDate());
