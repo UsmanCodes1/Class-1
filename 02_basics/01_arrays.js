b3e3aa58f919da3c2819540a0799ecbfa64feb7c
UsmanCodes1 <150183444+UsmanCodes1@users.noreply.github.com>
2023-11-25 17:51:56 +0000

    Array

--- 02_basics/01_arrays.js
+++ 02_basics/01_arrays.js
@@ -8,17 +8,17 @@ const myArr2 = new Array(1, 2, 3, 4)
 
 // Array methods
 
-// myArr.push(6)
-// myArr.push(7)
-// myArr.pop()
+// myArr2.push(9)
+// myArr.push(7) Add 7 in array
+// myArr2.pop()  Delete last number
 
-// myArr.unshift(9)
-// myArr.shift()
+// myArr.unshift(9) Add 9 in starting
+// myArr.shift()  Remove starting first number
+// console.log(myArr)
+// console.log(myArr.includes(5)); Give the answer that the 5 is in the array or not. Give the answer in True or False.
+// console.log(myArr.indexOf(3)); If 3 is in the Array give answer 3 and not give answer -1
 
-// console.log(myArr.includes(9));
-// console.log(myArr.indexOf(3));
-
-// const newArr = myArr.join()
+// const newArr = myArr.join() Convert in to string and alo binding in group.
 
 // console.log(myArr);
 // console.log( newArr);
@@ -29,11 +29,8 @@ const myArr2 = new Array(1, 2, 3, 4)
 console.log("A ", myArr);
 
 const myn1 = myArr.slice(1, 3)
-
 console.log(myn1);
 console.log("B ", myArr);
-
-
 const myn2 = myArr.splice(1, 3)
 console.log("C ", myArr);
 console.log(myn2);
