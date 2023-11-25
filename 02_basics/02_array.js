fb3d1271571cd9fca28839d139fb77d51e0fcd46
UsmanCodes1 <150183444+UsmanCodes1@users.noreply.github.com>
2023-11-25 18:08:57 +0000

    Array

--- 02_basics/02_array.js
+++ 02_basics/02_array.js
@@ -9,19 +9,22 @@ const dc_heros = ["superman", "flash", "batman"]
 // const allHeros = marvel_heros.concat(dc_heros)
 // console.log(allHeros);
 
-const all_new_heros = [...marvel_heros, ...dc_heros]
+// const all_new_heros = [...marvel_heros, ...dc_heros] using spread method for combining two array in to one array.
 
 // console.log(all_new_heros);
 
 const another_array = [1, 2, 3, [4, 5, 6], 7, [6, 7, [4, 5]]]
 
 const real_another_array = another_array.flat(Infinity)
+// We can use flat for combining sub array in to one array
 console.log(real_another_array);
 
 
 
 console.log(Array.isArray("Hitesh"))
+// TO confirm that it is array are not.
 console.log(Array.from("Hitesh"))
+// Convert in to array.
 console.log(Array.from({name: "hitesh"})) // interesting
 
 let score1 = 100
@@ -29,3 +32,4 @@ let score2 = 200
 let score3 = 300
 
 console.log(Array.of(score1, score2, score3));
+// Array.of is use for convert two are more value into one array.
